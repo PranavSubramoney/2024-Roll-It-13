0ce5cd9c02ce221a2449be8492b5457e23c29ca5
PranavSubramoney <158997160+PranavSubramoney@users.noreply.github.com>
2024-02-18 15:34:22 +1300

    working rounds (computer not able to pass yet)

--- C_05_rounds_v1.py
+++ C_05_rounds_v1.py
@@ -4,13 +4,14 @@ import random
 # generates an integer between 0 and 6
 # to simulate a roll of a die
 def roll_die():
-    result = random.randint(1, 6)
-    return result
-
+    roll_result = random.randint(1, 6)
+    return roll_result
 
 # rolls two dice and returns total and whether we
 # had a double roll
-def two_rolls():
+
+
+def two_rolls(who):
     double_score = "no"
 
     # roll two dice
@@ -22,12 +23,12 @@ def two_rolls():
         double_score = "yes"
 
     # Find the total points (so far)
-    user_points = roll_1 + roll_2
+    first_points = roll_1 + roll_2
 
     # Show the user the result
-    print(f"Die 1: {roll_1} \t Die 2: {roll_2}")
+    print(f"{who}: {roll_1} & {roll_2} - Total: {first_points}")
 
-    return user_points, double_score
+    return first_points, double_score
 
 
 # Main routine
@@ -35,25 +36,23 @@ print("Press <enter> to begin this round: ")
 input()
 
 # Get initial dice rolls for user
-user_first = two_rolls()
+user_first = two_rolls("User")
 user_points = user_first[0]
 double_points = user_first[1]
 
 # Tell the user if they are eligible for double points
-if double_points == "no":
-    double_feedback = ""
-else:
-    double_feedback = "If you win this round, you gain double points!"
+if double_points == "yes":
+    print("If you win this round, you gain double points!")
 
 # output initial move results
-print(f"You rolled a total of {user_points}.  {double_feedback}")
-print()
+# print(f"You rolled a total of {user_points}.  {double_feedback}")
+# print()
 
 # Get initial dice rolls for computer
-computer_first = two_rolls()
+computer_first = two_rolls("Computer")
 computer_points = computer_first[0]
 
-print(f"The computer rolled a total of {computer_points}.")
+# print(f"The computer rolled a total of {computer_points}.")
 
 # loop (while both user / computer have <= 13 points)...
 while computer_points < 13 and user_points < 13:
@@ -80,33 +79,45 @@ while computer_points < 13 and user_points < 13:
         else:
             print(f"You rolled a {user_move} and now have a total of {user_points}.")
 
-    # print("\nPress <enter> to continue...")
-    # input()
-
     # Roll die for computer and update computer points
     computer_move = roll_die()
     computer_points += computer_move
-    print(f"The computer rolled a {computer_move}.  The computer"
-          f"now has {computer_points}.")
+
+    # checks computer has not gone over...
+    if computer_points > 13:
+        print(f"💥💥💥The computer rolled a {computer_move}, taking their points"
+              f" to {computer_points}. This is over 13 points so the computer loses!💥💥💥")
+        computer_points = 0
+        break
+
+    else:
+        print(f"The computer rolled a {computer_move}. The computer"
+              f" now has {computer_points}.")
 
     print()
+    # Tell user if they are winning, losing or if
     if user_points > computer_points:
-        result = "You are ahead"
+        result = "😊😊😊 You are ahead 😊😊😊"
+    elif user_points < computer_points:
+        result = "😲😲😲 The computer is ahead! 😲😲😲"
     else:
-        result = "The computer is ahead"
+        result = "👀👀It's currently a tie.👀👀"
 
-    print(f"***Round Update****: {result} ")
-    print(f"User Score: {user_points} \t | \t Computer Score: {computer_points}")
 
 # Outside loop - double user points if they won and are eligible
 
 # Show rounds result
 if user_points < computer_points:
-    print("Sorry - you lost this round and got no points "
+    print("🥲🥲🥲 Sorry - you lost this round and no points "
           "have been added to your total score. The computer's score has"
-          f"increased by {computer_points} points.")
+          f"increased by {computer_points} points. 🥲🥲🥲")
 
 # currently does ot include double points
 else:
-    print(f"Yay! You won the round and {user_points} points have"
-          f"been added to your score")
+    # Double user points if they are eligible
+    if double_points == "yes":
+        user_points *= 2
+
+    print()
+    print(f"👍👍👍 Yay! You won the round and {user_points} points have "
+          f"been added to your score 👍👍👍")
